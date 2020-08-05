c335e688e38fd8907ca7ba6921d3116ba9941aff
TechieDevice <avenger.148@gmail.com>
2020-08-05 12:48:23 +0700

    workflow test

--- SpiderWalker/tests/SpiderWalker_test.py
+++ SpiderWalker/tests/SpiderWalker_test.py
@@ -4,7 +4,7 @@ from SpiderWalker import parse_data
 
 class TestParsing:
     def test_parse_data(self):
-        """parse_data(html) search links in html and return links[http://...]"""
+        """parse_data(html) search links in html and return links[http://]"""
         with open("./tests/rawHTML.html", "r") as rawHTML:
             html_data = rawHTML.read()
         with open("./tests/rawLinks.txt", "r") as rawLinks:
